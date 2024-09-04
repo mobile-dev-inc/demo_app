04e758ad1401a8f03a039e89bb0ebaaa217a087b
Bartek Pacia <barpac02@gmail.com>
2024-09-04 18:41:43 +0200

    update NestingScreen

--- lib/nesting_screen.dart
+++ lib/nesting_screen.dart
@@ -12,49 +12,77 @@ class _NestingScreenState extends State<NestingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    const padding = EdgeInsets.symmetric(horizontal: 48, vertical: 64);
+    const padding = EdgeInsets.symmetric(horizontal: 40, vertical: 48);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Nesting test screen'),
       ),
-      body: Stack(
-        children: [
-          Semantics(
-            identifier: 'level-0',
-            explicitChildNodes: true,
-            child: Container(
-              padding: padding,
-              child: Stack(
-                children: [
-                  Semantics(
-                    identifier: 'level-1',
-                    explicitChildNodes: true,
-                    child: Container(
-                      color: Colors.blue,
-                      padding: padding,
-                      child: Stack(
-                        children: [
-                          Semantics(
-                            identifier: 'level-2',
-                            explicitChildNodes: true,
-                            child: Container(
-                              color: Colors.orange,
-                              padding: padding,
-                            ),
-                          ),
-                          const Text('Container at level 2'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Text('Container at level 1'),
-                ],
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: Stack(
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Text('left side'),
               ),
             ),
-          ),
-          const Text('Container at level 0'),
-        ],
+            const Align(
+              alignment: Alignment.topCenter,
+              child: Text('top side'),
+            ),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Text('right side'),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: RotatedBox(
+                quarterTurns: 2,
+                child: Text('bottom side'),
+              ),
+            ),
+            Semantics(
+              identifier: 'level-0',
+              explicitChildNodes: true,
+              child: Container(
+                padding: padding,
+                child: Stack(
+                  children: [
+                    Semantics(
+                      identifier: 'level-1',
+                      explicitChildNodes: true,
+                      child: Container(
+                        color: Colors.blue,
+                        padding: padding,
+                        child: Stack(
+                          children: [
+                            Semantics(
+                              identifier: 'level-2',
+                              explicitChildNodes: true,
+                              child: Container(
+                                color: Colors.orange,
+                                padding: padding,
+                              ),
+                            ),
+                            const Text('Container at level 2'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Text('Container at level 1'),
+                  ],
+                ),
+              ),
+            ),
+            const Text('Container at level 0'),
+          ],
+        ),
       ),
     );
   }
