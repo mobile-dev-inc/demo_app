02bc2386b4d1b309deb4af78adb5632e2436c818
Bartek Pacia <barpac02@gmail.com>
2024-09-09 11:00:24 +0200

    add dark mode support

--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
