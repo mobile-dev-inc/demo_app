97167153610daa60c00fb634a569ddd13a062807
Tarek Belkahia <tarek.belkahia@gmail.com>
2024-09-15 23:30:48 +0200

    Add input test screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demo_app/defects_screen.dart';
 import 'package:demo_app/form_screen.dart';
+import 'package:demo_app/input_screen.dart';
 import 'package:demo_app/issue_1619_repro.dart';
 import 'package:demo_app/issue_1677_repro.dart';
 import 'package:demo_app/nesting_screen.dart';
@@ -106,6 +107,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   );
                 },
                 child: const Text('Form Test'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => const InputScreen()),
+                  );
+                },
+                child: const Text('Input Test'),
               ),
               ElevatedButton(
                 onPressed: () {
