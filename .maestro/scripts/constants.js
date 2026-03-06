8ed98fd89daa528c2ce8225fb5b417af82dc378a
Dan Caseley <dan@mobile.dev>
2026-03-06 19:03:49 +0000

    Remove unused test files

--- .maestro/scripts/constants.js
+++
@@ -1,5 +0,0 @@
-output.speed = {
-  slow_speed: 50,
-  default_speed: 70,
-  fast_speed: 80,
-}
--- .maestro/scripts/setSearchTerm.js
+++
@@ -1,9 +0,0 @@
-// setSearchTerm.js
-
-output.searchTerm = 'blackberries'
-if (maestro.platform == 'android'){
-    output.searchTerm = 'robots'
-}
-if (maestro.platform == 'ios'){
-    output.searchTerm = 'apples'
-}
